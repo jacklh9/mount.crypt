4d8e6bb8b4a7b5bb7a20aeadc8f01de45113b1fc
Jack Hamilton <jack@jackhamilton.net>
2017-12-04 11:12:19 -0800

    Move unit tests to own dir; refactor unit tests

--- tests/mountcrypt_unittest.py
+++ tests/mountcrypt_unittest.py
@@ -1,19 +1,32 @@
-import unittest
+import sys, unittest
 from unittest.mock import patch
+
+# local modules
+sys.path.append("..")
 from mountcrypt import MountCrypt
 
-class MountCryptTest(unittest.TestCase):
+
+class MountCryptMethodTests(unittest.TestCase):
 
 	def setUp(self):
 		self.mc = MountCrypt()
-		self.mc.read_config("mountcrypt.ini")
+		self.mc.read_config("../mountcrypt.ini")
 		print ("setUp executed!")
 
 	def tearDown(self):
 		print ("tearDown executed!")
 
+	### TESTS ###
 
-### MOCK METHODS ###
+	def testVersion1(self):
+		obj = self.mc.version
+		cls = type("string class")
+		self.assertIsInstance(obj, cls, "Version not in string format")
+
+        
+class MountCryptTest(unittest.TestCase):
+
+	### MOCK METHODS ###
 
 	def mock_decrypt_volume(volume, uuid):
 		print("Mocking decryption of volume.")
@@ -28,20 +41,15 @@ class MountCryptTest(unittest.TestCase):
 	def mock_mount_volume(self, mount_point):
 		print("Mocking volume mount.")
 
-### TESTS ###
+	### TESTS ###
 
-	def testVersion1(self):
-		obj = self.mc.version
-		cls = type("string class")
-		self.assertIsInstance(obj, cls, "Version not in string format")
-
-	@patch('mountcrypt.MountCrypt.decrypt_volume', mock_decrypt_volume)		
+	@patch('mountcrypt.MountCrypt.decrypt_volume', mock_decrypt_volume)
 	@patch('mountcrypt.MountCrypt.mount_volume', mock_mount_volume)
 	@patch('subprocess.run', mock_subprocess_run)
 	@patch('pathlib.Path.exists', mock_path_exists)
 	def testRun(self):
 		mc = MountCrypt(interactive=False)
-		mc.read_config("./mountcrypt.ini")
+		mc.read_config("../mountcrypt.ini")
 		mc.mount_volumes()
 
 
